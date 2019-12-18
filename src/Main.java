5685fcc013b65c06d55c85a5c78ffa9ccca3b9ed
dokstorm <gado97@mail.ru>
2019-12-18 23:02:46 +0300

    Add files via upload

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,6 @@
+import java.io.*;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
-import java.util.Scanner;
 
 public class Main {
     public static List<Integer> R(String x) {
@@ -56,67 +55,76 @@ public class Main {
         return result.toString();
     }
 
-    public static int[][] fromGraph6toMatrix() {
-        Scanner scanner = new Scanner("input.txt");
-        String graph6 = scanner.nextLine();
+    public static int[][] fromGraph6toMatrix(String path) throws IOException {
+        File file = new File(path);
+        BufferedReader br = new BufferedReader(new FileReader(file));
+        String graph6 = br.readLine();
         List<Integer> d = new ArrayList<>();
 
         for (int i = 0; i < graph6.length(); i++) {
-            d.add((int)graph6.charAt(i) - 63);
+            d.add((int) graph6.charAt(i) - 63);
         }
 
+        int n = d.get(0);
+        d.remove(0);
+
         StringBuilder s = new StringBuilder();
-        for (int i = 0; i < d.size(); i++) {
-            StringBuilder sb = new StringBuilder(d.get(i));
-            while (sb.length() % 6 != 0) {
-                sb.append(0);
+        for (Integer integer : d) {
+            s.append(String.format("%8s", Integer.toBinaryString(integer))
+                    .replace(' ', '0').substring(2));
+        }
+        int[][] result = new int[n][n];
+        int count = 0;
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                result[j][i] = Integer.parseInt(String.valueOf(s.toString().charAt(count)));
+                result[i][j] = Integer.parseInt(String.valueOf(s.toString().charAt(count)));
+                count++;
             }
-            sb.reverse();
-            s.append(sb);
+        }
+        return result;
+    }
+
+    public static int[][] uniteGraphs(int[][] graph1, int[][] graph2) {
+        int n1 = graph1.length;
+        int n2 = graph2.length;
+        int[][] result = new int[n1 + n2][n1 + n2];
+        for (int i = 0; i < n1; i++) {
+            for (int j = 0; j < n1; j++) {
+                result[i][j] = graph1[i][j];
+            }
+        }
+        for (int i = 0; i < n2; i++) {
+            for (int j = 0; j < n2; j++) {
+                result[i + n1][j + n1] = graph2[i][j];
+            }
+        }
+        return result;
+    }
+
+    public static void printMatrix(int[][] matrix) {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix.length; j++) {
+                System.out.print(" " + matrix[i][j]);
+            }
+            System.out.println();
         }
     }
 
- /*   void reading(int& n, vector<vector<int> >& v)
-    {
-        ifstream fin("input.txt");
-        char c;
-        fin>>c>>c;
-        n = (int)c;
-        n -= 63;
+    public static void out(String graph6) throws IOException {
+        File file = new File("out.txt");
+        BufferedWriter bw = new BufferedWriter(new FileWriter(file));
+        bw.write(graph6);
+        bw.close();
+    }
 
-        vector<int> d;
-        while(fin>>c)
-            d.push_back((int)c-63);
-
-        string s="";
-        for (int i=0; i<(int)d.size(); i++)
-            s+=get_0_and_1(d[i]);
-
-        vector<vector<int> > vec(n, vector<int>());
-        for (int i=0; i<n*n; i++)
-            if ((int)s[i]-'0')
-                vec[i/n].push_back(i%n);
-
-        v = vec;
-        fin.close();
-    }*/
-
-
-    /*public static String readMatrix() {
-        Scanner scanner = new Scanner("input.txt");
-
-        while (scanner.hasNextLine()) {
-            List<Integer> result =
-            String line = scanner.nextLine().split(" "));
-        }
-    }*/
-
-    public static void main(String[] args) {
-        System.out.println(N(460175067));
-        System.out.println(R("1000101100011100"));
-
-        int[][] matrix = {{0, 1, 1, 1, 1, 0, 0}, {1, 0, 1, 1, 1, 0, 0}, {1, 1, 0, 1, 0, 0, 0}, {1, 1, 1, 0, 0, 0, 0}, {1, 1, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0, 1}, {0, 0, 0, 0, 0, 1, 0}};
-        System.out.println(toGraph6(matrix));
-
+    public static void main(String[] args) throws IOException {
+        int[][] graph1 = fromGraph6toMatrix("input1.txt");
+        int[][] graph2 = fromGraph6toMatrix("input2.txt");
+        int[][] result = uniteGraphs(graph1, graph2);
+        printMatrix(result);
+        String resultGraph6 = toGraph6(result);
+        System.out.println("Graph6 result: " + resultGraph6);
+        out(toGraph6(result));
     }
 }
